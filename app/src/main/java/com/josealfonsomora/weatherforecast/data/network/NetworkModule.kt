923d1ebf6ca19faf5e64f5353bb57f34233facad
josealfonsomora <josealfonsomora@gmail.com>
2021-02-14 11:59:40 +0100

    Add certificate pinning

--- app/src/main/java/com/josealfonsomora/weatherforecast/data/network/NetworkModule.kt
+++ app/src/main/java/com/josealfonsomora/weatherforecast/data/network/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -25,7 +26,14 @@ class NetworkModule {
     }
 
     @Provides
-    fun provideOkHttp(interceptor: HttpLoggingInterceptor): OkHttpClient { // TODO: Add sslpinning
+    fun provideOkHttp(interceptor: HttpLoggingInterceptor): OkHttpClient {
+
+        val certificatePinner = CertificatePinner.Builder()
+            .add("*.openweathermap.org", "sha256/axmGTWYycVN5oCjh3GJrxWVndLSZjypDO6evrHMwbXg=") //CN=*.openweathermap.org
+            .add("*.openweathermap.org", "sha256/4a6cPehI7OG6cuDZka5NDZ7FR8a60d3auda+sKfg4Ng=")
+            .add("*.openweathermap.org", "sha256/x4QzPSC810K5/cMjb05Qm4k3Bw5zBn4lTdO/nEW/Td4=")
+            .build()
+
         return OkHttpClient.Builder()
             .connectTimeout(10, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
@@ -37,6 +45,7 @@ class NetworkModule {
                     .addQueryParameter("appid", "4cb14ee5bcb95c329c5c6468c540f38f")
                 chain.proceed(newRequest.url(urlBuilder.build()).build())
             }
+            .certificatePinner(certificatePinner)
             .build()
     }
 
